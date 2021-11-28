d7f4f042af7e361042aade2aa772b1e3376b6800
Zokhidjon <zohidjon.net@gmail.com>
2021-11-28 15:02:22 +0500

    Random Array
    Task 1 28.11

---
+++ Auto QA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Auto_QA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int min = 0;
+            int max = 28;
+
+            int[] arrays = new int[5];
+
+            Random randarr = new Random();
+            
+            
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                arrays[i] = randarr.Next(min, max);
+                
+                Console.WriteLine(arrays[i]);
+                
+            }
+            Array.Sort(arrays);
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                Console.Write(arrays[i] + " ");
+                //or we can use Console.WriteLine(arrays[i]);
+            }
+
+        }
+    }
+}
